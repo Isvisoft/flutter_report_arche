b49f174beeb387df85263ec64499e28f8866c5e6
Ismael Monje <imonje@isvisoft.com>
2020-05-28 10:11:47 +0200

    first commit

---
+++ lib/flutterreportarche.dart
@@ -0,0 +1,28 @@
+library flutterreportarche;
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
+
+class ReportService {
+  var token = "";
+
+  ReportService(String token) {
+    this.token = token;
+  }
+
+  downloadReport(Map data, String reportName, bool open) async {
+    var url = 'https://api.arche.isvisoft.com/report';
+    var response = await http.post(url, body: jsonEncode(data), headers: {'token': token, 'Content-Type': "application/json"});
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    String path = appDocDir.path + reportName;
+    new File(path).writeAsBytesSync(response.bodyBytes);
+
+    if (open) {
+      OpenFile.open(path);
+    }
+  }
+}
